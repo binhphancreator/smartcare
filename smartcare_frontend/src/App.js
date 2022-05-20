70dac0e13f57e7e217af68c4d68d53a705071368
binhphancreator <tienkhach19@gmail.com>
2022-05-20 18:25:06 +0700

    add antdesign and remove tailwind

--- smartcare_frontend/src/App.js
+++ smartcare_frontend/src/App.js
@@ -1,7 +1,14 @@
+import { Menu } from 'antd';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Khai phá dữ liệu
-    </h1>
+    <Menu
+        style={{ width: 256 }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline">
+      <Menu.Item key="1">Option 1</Menu.Item>
+      <Menu.Item key="2">Option 2</Menu.Item>
+    </Menu>
   )
 }
