e4aa8852a58a6a22c4c541e1684b3549e821c567
myusername <yenliot4311@gmail.com>
2022-06-11 15:50:44 +0700

    form

--- smartcare_frontend/src/components/FormDashboard.js
+++ smartcare_frontend/src/components/FormDashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import {
   Button,
   Cascader,
@@ -20,110 +21,137 @@ export default function FormDashboard() {
     }
     return arr;
   }
+
   return (
-    <div className="dashboard">
+    <div className="dashboard" >
       <div>
+      <h1 className="form_intro">PHIẾU ĐIỀN PHỎNG VẤN</h1>
         <Form layout="vertical" className="form">
           <Form.Item
-            label="Chỉ số BMI của bạn là bao nhiêu?"
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Chỉ số BMI của bạn là bao nhiêu?</p>}
             className="form_select"
           >
-            <InputNumber className="form_select"></InputNumber>
+            <InputNumber style={{ width: '100%' }} className="form_select_chirlden"></InputNumber>
           </Form.Item>
 
           <Form.Item
-            label="Bạn có thói quen hút thuốc không?"
+      
+            label = { 
+              <p style={{fontSize: "16px", fontWeight: "bold",paddingLeft:"20px", paddingTop:"10px"}}>Bạn có thói quen hút thuốc không?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item
-            label="Bạn có thói quen uống rượt bia không?"
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold", paddingLeft:"20px", paddingTop:"10px"}}>Bạn có thói quen uống rượt bia không?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item
-            label=" Bạn đã bao giờ bị ngất? (đột quỵ)?"
+            
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn đã bao giờ bị ngất? (đột quỵ)?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
 
           <Form.Item
-            label="Hãy ước lượng trong vòng 30 ngày trở lại đây, từng có bao nhiêu ngày bạn cảm thấy không khoẻ?"
+
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Hãy ước lượng trong vòng 30 ngày trở lại đây, từng có bao nhiêu ngày bạn cảm thấy không khoẻ?</p>}
             className="form_select"
           >
             <Select>
               {createrange(30).map((item, key) => {
-                return <Select.Option value={item}>{item} </Select.Option>;
+                return <Select.Option value={item} className="form_select_chirlden">{item} </Select.Option>;
               })}
             </Select>
           </Form.Item>
           <Form.Item
-            label=" Hãy ước lượng trong vòng 30 ngày trở lại đây, từng có bao nhiêu ngày tinh thần bạn không tốt??"
+  
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Hãy ước lượng trong vòng 30 ngày trở lại đây, từng có bao nhiêu ngày tinh thần bạn không tốt?</p>}
             className="form_select"
           >
-            <Select>
+            <Select >
               {createrange(30).map((item, key) => {
-                return <Select.Option value={item}>{item} </Select.Option>;
+                return <Select.Option value={item} className="form_select_chirlden">{item} </Select.Option>;
               })}
             </Select>
           </Form.Item>
           <Form.Item
-            label="Bạn có gặp khó khăn khi đi lại hoặc leo cầu thang?"
+           
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn có gặp khó khăn khi đi lại hoặc leo cầu thang?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item label="Bạn thuộc phái nam hay nữ?" className="form_select">
-            <Radio.Group>
+          <Form.Item 
+         
+           label = {
+            <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn thuộc phái nam hay nữ?</p>}
+           className="form_select">
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Nam </Radio>
               <Radio value={0}> Nữ </Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item label="Sắc tộc của bạn là gì?" className="form_select">
+          <Form.Item 
+          
+          label = {
+            <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Sắc tộc của bạn là gì?</p>}
+          className="form_select">
             <Select>
-              <Select.Option value={0}>Anh-Điêng </Select.Option>  
-              <Select.Option value={1}>Châu Á </Select.Option>
-              <Select.Option value={2}>Châu Phi </Select.Option>
-              <Select.Option value={3}>gốc Tây Ban Nha </Select.Option>
-              <Select.Option value={5}>Châu Âu</Select.Option>
-              <Select.Option value={4}>Khác... </Select.Option>
+              <Select.Option value={0} className="form_select_chirlden">Anh-Điêng </Select.Option>  
+              <Select.Option value={1} className="form_select_chirlden">Châu Á </Select.Option>
+              <Select.Option value={2} className="form_select_chirlden">Châu Phi </Select.Option>
+              <Select.Option value={3} className="form_select_chirlden">gốc Tây Ban Nha </Select.Option>
+              <Select.Option value={5} className="form_select_chirlden">Châu Âu</Select.Option>
+              <Select.Option value={4} className="form_select_chirlden">Khác... </Select.Option>
             </Select>
           </Form.Item>
           <Form.Item
-            label="Bạn đã từng bị tiểu đường chưa?"
+            
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn đã từng bị tiểu đường chưa?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
+              <Radio value={1}> Có </Radio>
+              <Radio value={0}> Không </Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item 
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn có thói quen tập thể dục?</p>}
+            className="form_select"
+          >
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item
-            label="Bạn có thói quen tập thể dục?"
-            className="form_select"
-          >
-            <Radio.Group>
-              <Radio value={1}> Có </Radio>
-              <Radio value={0}> Không </Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item
-            label="Nhìn chung sức khoẻ của bạn như thế nào?"
+            
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Nhìn chung sức khoẻ của bạn như thế nào?</p>}
             className="form_select"
           >
             <Select>
@@ -133,7 +161,9 @@ export default function FormDashboard() {
             </Select>
           </Form.Item>
           <Form.Item
-            label="Một ngày bạn ngủ bao nhiêu giờ?"
+        
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Một ngày bạn ngủ bao nhiêu giờ?</p>}
             className="form_select"
           >
             <Select>
@@ -143,31 +173,39 @@ export default function FormDashboard() {
             </Select>
           </Form.Item>
           <Form.Item
-            label="Bạn đã từng bị tiểu đường chưa?"
+         
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn đã từng bị tiểu đường chưa?</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item
-            label="Bạn có từng bị bệnh thận? (không bao gồm sỏi, nhiễm trùng, đái dầm)"
+          
+            label = {
+              <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn có từng bị bệnh thận? (không bao gồm sỏi, nhiễm trùng)</p>}
             className="form_select"
           >
-            <Radio.Group>
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item label="Bạn có từng bị ung thư da?" className="form_select">
-            <Radio.Group>
+          <Form.Item 
+              label = {
+                <p style={{fontSize: "16px", fontWeight: "bold" , paddingLeft:"20px", paddingTop:"10px"}}>Bạn có từng bị ung thư da?</p>} className="form_select">
+            <Radio.Group className="form_select_chirlden">
               <Radio value={1}> Có </Radio>
               <Radio value={0}> Không </Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item className="submit">
-            <Button>Button</Button>
+          <Button type="primary" shape="round" style={{width:"120px", height:"40px "}}>
+        Submit
+      </Button>
           </Form.Item>
         </Form>
       </div>
--- smartcare_frontend/src/pages/Dashboard.js
+++ smartcare_frontend/src/pages/Dashboard.js
@@ -1,9 +1,13 @@
 import React from "react";
 import FormDashboard from "../components/FormDashboard"
+import DefaultLayout from "../layouts/DefaultLayout"
 export default function Dashboard() {
   return (
     <div>
+      <DefaultLayout>
       <FormDashboard/>
+      </DefaultLayout>
+      
     </div>
   );
 }
