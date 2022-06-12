167d6bb6643d4483353ecd55fb58b96a3bb4ba75
binhphancreator <tienkhach19@gmail.com>
2022-06-12 11:01:12 +0700

    collapse

---
+++ smartcare_frontend/src/components/Collapse.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+function Collapse(props) {
+  const [show, setShow] = useState(false)
+  const [height, setHeight] = useState(0)
+  const collapseRef = useRef(null)
+
+  useEffect(() => {
+    setHeight(collapseRef.current.scrollHeight)
+  }, [])
+
+  function toggleCollapse(e) {
+    setShow(!show)
+  }
+
+  return ( <>
+    <div className='collapse' onClick={toggleCollapse}>
+      {
+        props.icon &&
+        <button className="collapse-icon">
+          {props.icon}
+        </button>
+      }
+
+      <div className='collapse-content'>
+        <div className='collapse-title'>{props.title}</div>
+        <div className="collapse-body" ref={collapseRef} style={{height: show ? height : 0}}>{props.body}</div>
+      </div>
+
+      <button className="btn-collapse" style={{transform: show ? "rotate(180deg)" : "none"}}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"></path>
+        </svg>
+      </button>
+
+    </div>
+  </> )
+}
+
+export default Collapse
--- smartcare_frontend/src/pages/Home.js
+++ smartcare_frontend/src/pages/Home.js
@@ -1,6 +1,5 @@
-import AboutUsComponent from "../components/AboutUs";
 import HeaderIntro from "../components/HeaderIntro";
-import ProductIntro from "../components/ProductIntro";
+import Collapse from "../components/Collapse";
 import DefaultLayout from "../layouts/DefaultLayout";
 
 function Home() {
@@ -8,8 +7,31 @@ function Home() {
     <>
       <DefaultLayout>
         <HeaderIntro />
-        <ProductIntro />
-        <AboutUsComponent />
+
+        <div className="container">
+        </div>
+
+        <div className="container" style={{display: "flex", alignItems: "center"}}>
+          <div className="space-y-24" style={{width: "50%"}}>
+            <p className="badge">#OurValues</p>
+            <h1 style={{fontWeight: 600, fontSize: "2.25rem", lineHeight: "2.5rem", color: "#27355b"}}>Giá trị cốt lõi của Smartcare</h1>
+            <Collapse body = "Xem tin tức sức khỏe, các thông tin bổ ích hàng ngày." title = "Bản tin sức khỏe"  />
+            <Collapse body = "Xem tin tức sức khỏe, các thông tin bổ ích hàng ngày." title = "Dự đoán bệnh tim"  />
+            <Collapse body = "Xem tin tức sức khỏe, các thông tin bổ ích hàng ngày." title = "Health News"  />
+            <Collapse body = "Xem tin tức sức khỏe, các thông tin bổ ích hàng ngày." title = "Health News"  />
+          </div>
+          <div style={{width: "50%"}}>
+            <div style={{padding: "12px", display: "flex", justifyContent: "flex-end", alignItems: "center"}}>
+              <img
+                draggable="false"
+                src={require("../assets/images/intro.png")}
+                alt=""
+                style={{maxWidth: "80%"}}
+              />
+            </div>
+          </div>
+        </div>
+
       </DefaultLayout>
     </>
   );
