2e83bad32651c77fa705c8cca82235b31ff048b0
use25 <55612389+use25@users.noreply.github.com>
2022-06-16 23:29:40 +0700

    fix oauth

--- smartcare_backend/app/middlewares/oauth.py
+++ smartcare_backend/app/middlewares/oauth.py
@@ -13,19 +13,21 @@ def oauth(controller):
     def decorated(*args, **kwargs):
         token = None
 
-        # if "Authorization" in request.headers:
-        #   auth_token = request.headers["Authorization"]
-        #   if auth_token.find("Bearer ") == 0:
-        #     token = auth_token.replace("Bearer ", "").replace("  ", " ").strip(" ")
+        if "Authorization" in request.headers:
+            auth_token = request.headers["Authorization"]
+            if auth_token.find("Bearer ") == 0:
+                token = auth_token.replace(
+                    "Bearer ", "").replace("  ", " ").strip(" ")
 
-        # if token == None or len(token) == 0:
-        #   return respondError(errors="Unauthorized", status=401)
+        if token == None or len(token) == 0:
+            return respondError(errors="Unauthorized", status=401)
 
-        # data = jwt.decode(token, os.getenv("JWT_SECRET_KEY"), algorithms=["HS256"])
-        # if datetime.now() > datetime.fromisoformat(data['expires_at']):
-        #   return respondError(errors="Token expired", status=401)
+        data = jwt.decode(token, os.getenv(
+            "JWT_SECRET_KEY"), algorithms=["HS256"])
+        if datetime.now() > datetime.fromisoformat(data['expires_at']):
+            return respondError(errors="Token expired", status=401)
 
-        user = User.objects(id='62ab517e0b7ba3e86d8582bf').first()
+        user = User.objects(id=data['user_id']).first()
 
         return controller(user, *args, **kwargs)
 
