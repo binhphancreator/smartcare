c95ec6595f13e4ef3168196743c6f5b843cc1285
Subzero <84495690+Combatman96@users.noreply.github.com>
2022-06-17 23:53:56 +0700

    new dataset

--- smartcare_backend/Model_Knn/test_model.py
+++ smartcare_backend/Model_Knn/test_model.py
@@ -1,13 +1,13 @@
 import Knn_model as knn
 
 #knn.BuidlKNN('smartcare_backend\Model_Knn\Dataset\heart_2020_cleaned_all_numeral.csv', 'smartcare_backend\Model_Knn\MODEL_KNN.sav', False)
-# knn.BuidlKNN('smartcare_backend\Model_Knn\Dataset\heart_failure_clinical_records_dataset.csv',
-#             'smartcare_backend\Model_Knn\MODEL_KNN_DOCTOR.sav', True)
+knn.BuidlKNN('smartcare_backend\Model_Knn\Dataset\heart_doctor_cleaned_data_new.csv',
+            'smartcare_backend\Model_Knn\MODEL_KNN_DOCTOR.sav', True)
 
-# features = [
-#     30.23, 1, 0, 0, 6, 2, 1, 0, 11, 5, 1, 1, 1, 8, 0, 1, 0
-# ]
-features = [53,0,63,1,60,0,368000,0.8,135,1,0,22]
+# # features = [
+# #     30.23, 1, 0, 0, 6, 2, 1, 0, 11, 5, 1, 1, 1, 8, 0, 1, 0
+# # ]
+features = [41,0,0,1,0,1,0,332,124,88,31.31,65,84]
 result = knn.makePrediction(
-    './MODEL_KNN_DOCTOR.sav', features, True)
+    'smartcare_backend\Model_Knn\MODEL_KNN_DOCTOR.sav', features, True)
 print(result)
